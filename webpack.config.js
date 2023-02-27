30ac40260a8ac18dbc88869327a9e2d5b9d84d70
Paul Koski <pwkoski@gmail.com>
2023-02-27 12:14:32 -0500

    1. Alter webconfig file to work with .jsx instead of .js

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, "src", "index.jsx"),
   output: {
     path:path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
-        test: /\.?js$/,
+        test: /\.?jsx$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
